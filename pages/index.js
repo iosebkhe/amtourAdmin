a06f563d9e2cece00796e4c07eca791f5c8e5174
iosebkhe <ioseb.khetsuriani@yahoo.com>
2024-07-05 05:52:28 +0400

    fixed page title

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Layout from "@/components/Layout";
 import { useSession } from "next-auth/react";
+import Head from "next/head";
 import Link from "next/link";
 
 export default function Home() {
   const { data: session } = useSession();
   return (
     <Layout>
+      <Head>
+        <title>Amtour Admin</title>
+      </Head>
       <h2 className="text-primary text-2xl text-center">
         მოგესალმები, <strong>{session?.user?.name}</strong>
       </h2>
